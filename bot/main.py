30712206fa07e76105a3adf092e670c9a8ea462f
Kevin <kevingu02@hotmail.com>
2025-01-31 20:34:34 -0500

    code cleanup

--- bot/main.py
+++ bot/main.py
@@ -2,131 +2,149 @@ import os
 import discord
 import random
 import json
-import difflib
 import statistics
+import difflib
+from discord.ext import commands
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Setup intents
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-containerFileList = ["Supercontainer.json", "SantasGift2021.json", "SantasBigGift2021.json", "SantasMegaGift2021.json", "SantasGift2022.json", "SantasBigGift2022.json", "SantasMegaGift2022.json", "SantasGift2023.json", "SantasBigGift2023.json", "SantasMegaGift2023.json", "SantasGift2024.json", "SantasMegaGift2024.json"] #list of container data .json file names
-containerDataList = [] #container data
-containerNickNameList = [] #container nicknames
-containerNameList = [] #container names
-for filename in containerFileList:
-    f = open(filename, "r", encoding="utf-8")
-    data = json.load(f)
-    containerDataList.append(data)
-    containerNickNameList.append(data["nickname"])
-    containerNameList.append(data["name"])
-    f.close()
+# Create a bot instance with command prefix "!"
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+# Load container data
+container_files = [
+    "Supercontainer.json", "SantasGift2021.json", "SantasBigGift2021.json", 
+    "SantasMegaGift2021.json", "SantasGift2022.json", "SantasBigGift2022.json", 
+    "SantasMegaGift2022.json", "SantasGift2023.json", "SantasBigGift2023.json", 
+    "SantasMegaGift2023.json", "SantasGift2024.json", "SantasMegaGift2024.json"
+]
+
+container_data = {}
+for filename in container_files:
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        container_data[data["nickname"].lower()] = data  # Store by lowercase nickname for easy lookup
+
+# Bot Ready Event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-@client.event
-async def on_message(message):
-    if message.author == client.user: #ignore messages from itself
+    print(f'{bot.user.name} has connected to Discord!')
+
+# Command: Collection Simulation
+@bot.command(name="collection")
+async def collection(ctx, n: int, k: int, d: int, c: int):
+    """Simulates the number of containers needed to complete a collection."""
+    
+    # Input validation
+    if n > 100 or n <= 0:
+        await ctx.send("There must be between 1 and 100 items in a collection.")
         return
-    if '!collection'.lower() in message.content.lower() and message.content[0] == "!":
-        content = message.content.split(" ")
-        if len(content) == 5 and content[1].isdigit() and content[2].isdigit() and content[3].isdigit() and content[4].isdigit():
-            runs = 100000
-            n = int(content[1])
-            k = int(content[2])
-            d = int(content[3])
-            c = int(content[4])
-            if n > 100:
-                await message.channel.send("There can not be more than 100 items in a collection.")
-            elif n <= 0:
-                await message.channel.send("There must be at least one item in a collection.")
-            elif k > n:
-                await message.channel.send("There can not be more items currently owned than there are total items in a collection.")
-            elif k < 0:
-                await message.channel.send("There can not be less than 0 items owned.")
-            elif d < 0:
-                await message.channel.send("There can not be less than 0 duplicates owned.")
-            elif c <= 0:
-                await message.channel.send("The conversion cost of duplicates to an item must be 1 or higher.")
+    if k > n or k < 0:
+        await ctx.send("The number of owned items must be between 0 and the total number of items.")
+        return
+    if d < 0:
+        await ctx.send("Duplicates cannot be negative.")
+        return
+    if c <= 0:
+        await ctx.send("Conversion cost of duplicates must be 1 or higher.")
+        return
+
+    runs = 100000
+    total_containers = 0
+    results = []
+
+    for _ in range(runs):
+        collection = [-1] * (n - k) + [1] * k
+        containers = 0
+        dupes = d
+        empties = n - k
+
+        while empties > 0:
+            containers += 1
+            index = random.randint(0, n - 1)
+            if collection[index] == 1:
+                dupes += 1
             else:
-                totalcontainers = 0
-                array = []
-                for i in range(runs):
-                    collection = (n - k) * [-1] + (k) * [1]
-                    containers = 0
-                    dupes = d
-                    empties = n - k
-                    while(empties != 0):
-                        containers += 1
-                        index = int(n * random.random())
-                        if collection[index] == 1:
-                            dupes += 1
-                        else:
-                            collection[index] = 1
-                            empties -= 1
-                        if dupes / c >= empties:
-                            break
-                    totalcontainers += containers
-                    array.append(containers)
-                array.sort(reverse=True)
-                stdev = statistics.pstdev(array)
-                collectionmessage = "```"
-                collectionmessage += "\nOn average, you will need to open " + str(round(totalcontainers / runs, 1)) + " containers."
-                collectionmessage += "\nThe standard deviation for number of containers needed to complete the collection is " + str(round(stdev, 1)) + "."
-                collectionmessage += "\nYou can reasonably expect to open between " + str(array[int(0.9985 * runs)]) + " and " + str(array[int(0.0015 * runs)]) + " containers to complete the collection."
-                collectionmessage += "\nAt 0.15 percentile:  " + str(array[int(0.0015 * runs)])
-                collectionmessage += "\nAt 2.5 percentile:   " + str(array[int(0.025 * runs)])
-                collectionmessage += "\nAt 16 percentile:    " + str(array[int(0.16 * runs)])
-                collectionmessage += "\nAt the median:       " + str(array[int(0.5 * runs)])
-                collectionmessage += "\nAt 84 percentile:    " + str(array[int(0.84 * runs)])
-                collectionmessage += "\nAt 97.5 percentile:  " + str(array[int(0.975 * runs)])
-                collectionmessage += "\nAt 99.85 percentile: " + str(array[int(0.9985 * runs)]) + "```"
-                await message.channel.send(collectionmessage)
-        else:
-            await message.channel.send("Please use the format: !collection [total number of collection items] [current number of collection items owned] [current number of duplicates owned] [number of duplicates needed to purchase one collection item]")
-    if '!help'.lower() in message.content.lower() and message.content[0] == "!" or message.content.lower() == "!open":
-        await message.channel.send("Use !open [container name]. Always assumes no unique items are currently owned and will not account for duplicates and pity successes when opening one container after another. Available containers: " + ", ".join(sorted(containerNickNameList)))
-    if '!open'.lower() in message.content.lower() and message.content[0] == "!" and message.content.lower() != "!open":
-        containerData = containerDataList[containerNickNameList.index(difflib.get_close_matches(message.content.lower().partition(' ')[2], containerNickNameList, n=1, cutoff=0)[0])] #finds closest container name to input
-        
-        embeded = discord.Embed(title=containerData["name"],color=discord.Color.from_str(containerData["color"]))
-        
-        dropPool = containerData["drops"]
-        roll = random.random() * 100
-        dropIndex = -1 # current drop
-        lastRate = 100
+                collection[index] = 1
+                empties -= 1
 
-        if "rate" in dropPool[0].keys(): # the current drop pool has manual rates
-            while roll >= 0:
-                dropIndex += 1
-                roll -= dropPool[dropIndex]["rate"] / 100 * lastRate
-                tempRate = dropPool[dropIndex]["rate"] / 100 * lastRate
-        else: # the current drop pool has evenly distributed rates
-            dropIndex = roll // lastRate * len(dropPool)
-            tempRate = lastRate / len(dropPool)
+            if dupes // c >= empties:
+                break
 
-        while "drops" in dropPool[dropIndex].keys():
-            dropPool = dropPool[dropIndex]["drops"]
-            lastRate = tempRate
-            roll += lastRate
-            dropIndex = -1
+        total_containers += containers
+        results.append(containers)
 
-            if "rate" in dropPool[0].keys(): # the current drop pool has manual rates
-                while roll >= 0:
-                    dropIndex += 1
-                    roll -= dropPool[dropIndex]["rate"] / 100 * lastRate
-                    tempRate = dropPool[dropIndex]["rate"] / 100 * lastRate
-            else: # the current drop pool has evenly distributed rates
-                dropIndex = int(roll // (lastRate / len(dropPool)))
-                tempRate = lastRate / len(dropPool)
-        drop = dropPool[dropIndex]
+    results.sort(reverse=True)
+    stdev = statistics.pstdev(results)
 
-        embeded.description = drop["name"]
-        if "link" in drop.keys():
-            embeded.set_image(url=drop["link"])
+    # Generate response
+    percentiles = [0.0015, 0.025, 0.16, 0.5, 0.84, 0.975, 0.9985]
+    percentile_values = [results[int(p * runs)] for p in percentiles]
 
-        await message.channel.send(embed=embeded)
+    response = f"```\n"
+    response += f"On average, you will need to open {total_containers / runs:.1f} containers.\n"
+    response += f"Standard deviation: {stdev:.1f} containers.\n"
+    response += f"Expected range: {percentile_values[6]} - {percentile_values[0]} containers.\n"
+    
+    labels = ["0.15%", "2.5%", "16%", "50% (Median)", "84%", "97.5%", "99.85%"]
+    for label, value in zip(labels, percentile_values):
+        response += f"At {label} percentile: {value}\n"
+    
+    response += "```"
+    await ctx.send(response)
 
-client.run(TOKEN)
+# Command: Help
+@bot.command(name="help")
+async def help_command(ctx):
+    """Provides help message for available commands."""
+    await ctx.send(f"Use `!open [container name]`. Available containers: {', '.join(sorted(container_data.keys()))}")
+
+# Command: Open Container
+@bot.command(name="open")
+async def open_container(ctx, *, container_name: str):
+    """Simulates opening a container and getting a random drop."""
+    
+    # Find best matching container name
+    matched_name = difflib.get_close_matches(container_name.lower(), container_data.keys(), n=1, cutoff=0)
+    if not matched_name:
+        await ctx.send("Invalid container name. Use `!help` to see available containers.")
+        return
+
+    container = container_data[matched_name[0]]
+    
+    # Embed for Discord message
+    embed = discord.Embed(title=container["name"], color=discord.Color.from_str(container["color"]))
+    
+    drop_pool = container["drops"]
+    roll = random.uniform(0, 100)
+    drop_index = 0
+    last_rate = 100
+
+    # Determine the drop
+    while "drops" in drop_pool[drop_index]:
+        drop_pool = drop_pool[drop_index]["drops"]
+        roll = random.uniform(0, 100)
+        drop_index = 0
+
+    if "rate" in drop_pool[0]:  # Manual rate distribution
+        while roll >= 0:
+            roll -= drop_pool[drop_index]["rate"] / 100 * last_rate
+            drop_index += 1
+    else:  # Even distribution
+        drop_index = int(roll // (last_rate / len(drop_pool)))
+
+    drop = drop_pool[drop_index]
+
+    # Embed the result
+    embed.description = drop["name"]
+    if "link" in drop:
+        embed.set_image(url=drop["link"])
+
+    await ctx.send(embed=embed)
+
+# Run the bot
+bot.run(TOKEN)
